7bf96d66910febf6bd3993a808256a61593d880b
chry4n <rchen72@buffalo.edu>
2024-11-09 22:49:55 -0500

    numbering mode set

--- display.py
+++ display.py
@@ -1,7 +1,8 @@
 from RPLCD import CharLCD
+import RPi.GPIO as GPIO
 
 
 
 
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23], numbering_mode=GPIO.BOARD)
 lcd.write_string(u"Hello world")
