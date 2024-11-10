0e34879c8cb73661510b710c345ba8184000589b
chry4n <rchen72@buffalo.edu>
2024-11-10 00:04:48 -0500

    split button functions

--- button_listeners.py
+++ button_listeners.py
@@ -5,14 +5,20 @@ from gpiozero import Button
 reg_button = Button(21)
 ver_button = Button(20)
 
-def buttons(button_number):
-    if button_number == 21: # register case
-        on_register_press()
-    elif button_number == 20:
-        on_verify_press()
+def button1():
+    on_register_press
+    
+def button2():
+    on_verify_press
 
-reg_button.when_pressed = buttons(21)
-ver_button.when_pressed = buttons(20)
+# def buttons(button_number):
+#     if button_number == 21: # register case
+#         on_register_press()
+#     elif button_number == 20:
+#         on_verify_press()
+
+reg_button.when_pressed = button1
+ver_button.when_pressed = button2
 
 while True:
     pass
