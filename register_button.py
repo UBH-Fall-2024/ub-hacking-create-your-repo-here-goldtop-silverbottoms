f9c74b088aa4384f6ba8fbd99c91ad5f7ba21d00
chry4n <rchen72@buffalo.edu>
2024-11-10 04:14:22 -0500

    fixing no face verification

--- register_button.py
+++ register_button.py
@@ -1,8 +1,10 @@
 import subprocess
+# from servo_power import mu
 
-
+# mu = True
 def on_register_press():
     print("Register button pressed!")
     subprocess.call(["python3", "./register/face.py"])
-    subprocess.call(["python3", "./register/encoder.py"])
-    
+    result = subprocess.call(["python3", "./register/encoder.py"])
+    # mu = False
+    return result
