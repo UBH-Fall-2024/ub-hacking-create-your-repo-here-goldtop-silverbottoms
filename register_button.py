593872bf11cbbb92f22846ee71c06c07e3467771
chry4n <rchen72@buffalo.edu>
2024-11-09 22:14:25 -0500

    register and verify buttons binded. hello world test liquid display

---
+++ register_button.py
@@ -0,0 +1,16 @@
+from gpiozero import Button, Device
+from gpiozero.pins.mock import MockFactory
+import subprocess
+
+
+# Enable Mock mode
+Device.pin_factory = MockFactory()
+
+button = Button() # put number of GPIO pin in function
+
+def on_register_press():
+    print("Register button pressed!")
+    subprocess.call(["python3", "./register/face.py"])
+    subprocess.call(["python3", "./register/encoder.py"])
+    
+button.when_activated = on_register_press
---
+++ verify_button.py
@@ -0,0 +1,33 @@
+from gpiozero import Button, Device, Servo
+from gpiozero.pins.mock import MockFactory
+from time import sleep
+import subprocess
+from RPLCD import CharLCD
+
+
+# Enable Mock mode
+Device.pin_factory = MockFactory()
+
+button = Button() # put number of GPIO pin in function
+servo = Servo()
+
+def on_verify_press():
+    print("Verify button pressed!")
+    subprocess.call(["python3", "./verify/facial.py"])
+    result = subprocess.call(["python3", "./verify/compare.py"])
+    if result.returncode == 0:
+        # lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[])
+        # lcd.write_string(u"Hello world")
+        print("Face verified from button press!\nSpinning motor...")
+        for _ in range(5):
+            servo.max()
+            sleep(0.5)
+            servo.min()
+            sleep(0.5)
+        
+        servo.mid()
+        print("Motor stopped.")
+    else:
+        print("Face not verified from button press. Motor won't spin.")
+        
+button.when_activated = on_verify_press
